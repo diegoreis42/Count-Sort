ded4049236d3115597cdca4f08702452f948c173
Diego Reis <reisdiego144@gmail.com>
2025-03-16 01:39:31 -0300

    Clean up

--- ordenacao/main_count.c
+++ ordenacao/main_count.c
@@ -4,8 +4,6 @@
 #include <math.h>
 #include "ordenacao.h"
 
-
-
 int main(){
     char *arq[]  = {"../rand_files/100.txt", "../rand_files/1_000.txt", "../rand_files/10_000.txt", "../rand_files/100_000.txt", "../rand_files/1_000_000.txt"};
     char *vets[] = {"../vetoresOrdenados/counting/100Ord.txt", "../vetoresOrdenados/counting/1_000Ord.txt", "../vetoresOrdenados/counting/10_000Ord.txt", "../vetoresOrdenados/counting/100_000Ord.txt", "../vetoresOrdenados/counting/1_000_000Ord.txt"};
--- ordenacao/repetidor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-    g++ main.c ordenacao.c -o programa
-    time ./programa
-done
